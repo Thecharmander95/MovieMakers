47c19903f1fbd160d2268d113c1982cae1072afb
AMidili <tonywinslow@yahoo.com>
2018-01-31 20:57:42 -0500

    Finish generating movie scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :movies
-  get 'sites/home'
+  root 'sites#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
