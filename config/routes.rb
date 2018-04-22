834f80b95113d8fd25816159fe4dad01871a7f86
AMidili <tonywinslow@yahoo.com>
2018-04-22 13:01:30 -0400

    Create scene scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :movies
+  resources :scenes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
