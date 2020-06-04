8f1a1da8e1167e3a6d8c2e96b46603c680d4ca22
Leo Midili <leomidili07@gmail.com>
2020-06-03 20:05:22 -0400

    Finshed

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,6 +1,6 @@
 class Scene < ApplicationRecord
   validates :name, length: { minimum: 3}
-  validates :content, length: { minimum: 5}
+  validates :content, length: { minimum: 2}
 
   belongs_to :movie
 end
