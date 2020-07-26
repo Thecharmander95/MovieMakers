f28afb771c6e5f9b14a8328b6a4cb1a12437e47e
DJ967 <charmandermaster07@gmail.com>
2020-07-26 12:05:02 -0400

    migrations updates

--- db/migrate/20200530161934_rename_by_movies.rb
+++ db/migrate/20200530161934_rename_by_movies.rb
@@ -1,6 +1,5 @@
 class RenameByMovies < ActiveRecord::Migration[6.0]
   def change
-    remove_column :movies, :by, :string
     add_column :movies, :author, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_163057) do
+ActiveRecord::Schema.define(version: 2020_07_25_145009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 2020_07_12_163057) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "author"
+    t.string "slug"
+    t.index ["slug"], name: "index_movies_on_slug", unique: true
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
