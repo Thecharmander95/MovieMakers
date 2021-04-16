390429362bf6c7f60e070bf3584fe086c3b58708
DJ967 <charmandermaster07@gmail.com>
2021-04-16 18:28:30 -0400

    Merged db on delveoment removed unessary files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+    @users = User.by_newest
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: 'User deleted.'
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.avatar.attach(user_params[:avatar]) if user_params[:avatar].present?
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to root_path, notice: 'User was successfully updated.' }
+        format.json { render :index, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.friendly.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :phone, :body, :avatar)
+    end
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :validatable
+  # Following and Unfollowing
   has_many :movies, dependent: :destroy
+
+  has_one_attached :avatar, dependent: :destroy
+
+  validates :username, uniqueness: true, length: { minimum:1}
+
 end
